fc52212c104b6dcf408ef0c916f5bdfe9bfc60da
yuanyuanyuan <edwin@godblessyuan.com>
2017-11-20 10:04:50 +0800

    lession 3 4 5

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,6 @@
-
+import 'babel-polyfill';
 // import './class/lesson1.js';
-import './class/lession2.js';
+// import './class/lession2.js';
+// import './class/lession3.js';
+// import './class/lession4.js';
+import './class/lession5.js';
